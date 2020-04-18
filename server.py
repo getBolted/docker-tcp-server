069f537cf9b45c52a3855030f5d13547e87d731b
Alexsey Boltunov <42418759+getBolted@users.noreply.github.com>
2020-04-18 18:51:57 +0300

    JSONIZED

--- server.py
+++ server.py
@@ -1,30 +1,39 @@
-#Модуль socketserver для сетевого программирования
 from socketserver import *
 import urllib.request
 import json
 import requests
 import socket 
-#данные сервера
+import pandas as pd
+
+X = pd.read_csv(r'x_unix.csv')
+X = X.set_index('Unix')
+
+y = pd.read_csv(r'y_unix.csv')
+
+frontend_url = 'http://predictor-frontend.default.svc.cluster.local/live-data'
+predictor_url = 'http://xgboost.default.svc.cluster.local/get_predict'
 host = '0.0.0.0'
 port = 32000
 addr = (host,port)
 
-#обработчик запросов TCP подкласс StreamRequestHandler
 class MyTCPHandler(StreamRequestHandler):
-    response  = 'Pod {0} got message: '.format(socket.gethostname())
-    #функция handle делает всю работу, необходимую для обслуживания запроса. 
-    #доступны несколько атрибутов: запрос доступен как self.request, адрес как self.client_address, экземпляр сервера как self.server
-    def handle(self):     
-        self.data = self.request.recv(1024)
-        print ('client sent: '+str(self.data))
-        self.response += self.data.decode('utf-8')
-        #sndall - отправляет сообщение
-        self.request.sendall(bytes(self.response, 'utf-8'))
-
+	def handle(self): 
+#         TCP to index
+		self.data = self.request.recv(1024)
+		index = int(self.data.decode('utf-8'))
+		dict_pic = {
+            'obs_datetime':y.Unix[index],
+            'observed' : round(y.Aggregate[index], 5),
+            'pred_datetime': X.index[index]
+            }
+#		index to POST-predictor
+		headers = {'content-type': 'application/json'}
+		body = X.iloc[index].to_json(orient = 'index')
+		pred_response = requests.post(predictor_url, json=body, headers=headers)
+		dict_pic.update(json.loads(pred_response.text))
+#       predictor JSON to frontend
+		front_response = requests.post(frontend_url, json=dict_pic, headers=headers)
 if __name__ == "__main__":
-    
-    #Создаем экземпляр класса
-    server = TCPServer(addr, MyTCPHandler)
-    print('starting server... for exit press Ctrl+C')
-    #serve_forever - запускаем сервер
-    server.serve_forever()
+	server = TCPServer(addr, MyTCPHandler)
+	print('starting server... for exit press Ctrl+C')
+	server.serve_forever()
